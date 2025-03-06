15905fffa91f1ba08ba278698576918bf1b03df7
Anton <example@mail.ru>
2025-03-06 18:11:52 +0500

    add owner for recipients in admin panel

--- mailing/admin.py
+++ mailing/admin.py
@@ -16,7 +16,7 @@ class MailingAdmin(admin.ModelAdmin):
 
 @admin.register(Recipient)
 class RecipientAdmin(admin.ModelAdmin):
-    list_display = ('email', 'full_name', 'comment')
+    list_display = ('email', 'full_name', 'comment', 'owner')
     search_fields = ('email', 'full_name')
 
 
